96b517afbe75459272d007c0d5543476568fbfde
Larnzillotti <ngrecco@masonlive.gmu.edu>
2018-11-15 19:57:48 -0500

    update

--- P4_Collection/js/main.js
+++ P4_Collection/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     // run function on initial page load
-	
+
+	imageScroll();
 	// vPosH1();
 
 	// run function on resize of the window
@@ -10,9 +11,30 @@ $(document).ready(function() {
 
     // run function on scroll
     $(window).scroll(function() {
-    	imageScroll();
+    	
     });
 });
+
+function imageScroll() {
+//0-35, 35-75, 75+
+	$(window).on('scroll', function() {
+		var scrollTop = $(window).scrollTop();
+		var wh = $(document).height() - $(window).height();
+
+		var scrollTopPer = ((scrollTop*100)/wh);
+		//$('.scrollDis').text('You have scrolled the page by: ' + scrollTopPer +'percent');
+		console.log('You have scrolled the page by: ' + scrollTopPer +' percent');
+
+		if ((scrollTopPer > 0) && (scrollTopPer <= 35)) {
+			$('.img').attr("src","img/one.jpg");
+		} else if ((scrollTopPer > 35) && (scrollTopPer <= 75)) {
+			$('.img').attr("src","img/two.jpg");
+		} else if (scrollTopPer > 75) {
+			$('.img').attr("src","img/three.jpg");
+		}
+	})
+	
+}
 
 // mouse event link https://api.jquery.com/category/events/mouse-events/
 function demoFunction() {
@@ -36,51 +58,51 @@ function vPosH1() {
 	console.log(tP);
 }
 
-function jqCoordinates() {
+//function jqCoordinates() {
 
-	var leaveCount = 0;
+//	var leaveCount = 0;
 
-	$('.box').mousemove(function( e ) {
-		var offset = $(this).offset();
-  		var relativeX = (e.pageX - offset.left);
-  		var relativeY = (e.pageY - offset.top);
-  		var windowWidth = $(window).width();
- 		var windowHeight = $(window).height();
-  		var simpleWidth = ((relativeX / windowWidth) * 100).toFixed(2);
-		var simpleHeight = ((relativeY / windowHeight) * 100).toFixed(2);
-  		$('.coordinates').text("X: " + simpleWidth + "  Y: " + simpleHeight);
-  	});
+//	$('.box').mousemove(function( e ) {
+//		var offset = $(this).offset();
+//  		var relativeX = (e.pageX - offset.left);
+//  		var relativeY = (e.pageY - offset.top);
+//  		var windowWidth = $(window).width();
+// 		var windowHeight = $(window).height();
+//  		var simpleWidth = ((relativeX / windowWidth) * 100).toFixed(2);
+//		var simpleHeight = ((relativeY / windowHeight) * 100).toFixed(2);
+//  		$('.coordinates').text("X: " + simpleWidth + "  Y: " + simpleHeight);
+//  	});
   		
-	$('.box').click(function( e ) {
-		var offset = $(this).offset();
-  		var relativeX = (e.pageX - offset.left);
-  		var relativeY = (e.pageY - offset.top);
-  		var windowWidth = $(window).width();
- 		var windowHeight = $(window).height();
-  		var simpleWidth = ((relativeX / windowWidth) * 100).toFixed(2);
-		var simpleHeight = ((relativeY / windowHeight) * 100).toFixed(2);
-		var Red_Value = simpleWidth * 2.55;
-		var Blue_Value = simpleHeight * 2.55;
-  		$('.box').css("background-color", "rgb(" + Red_Value + ", 0, " + Blue_Value + ")");
-	});
+//	$('.box').click(function( e ) {
+//		var offset = $(this).offset();
+//		var relativeX = (e.pageX - offset.left);
+//		var relativeY = (e.pageY - offset.top);
+//		var windowWidth = $(window).width();
+//		var windowHeight = $(window).height();
+//		var simpleWidth = ((relativeX / windowWidth) * 100).toFixed(2);
+//		var simpleHeight = ((relativeY / windowHeight) * 100).toFixed(2);
+//		var Red_Value = simpleWidth * 2.55;
+//		var Blue_Value = simpleHeight * 2.55;
+//		$('.box').css("background-color", "rgb(" + Red_Value + ", 0, " + Blue_Value + ")");
+//	});
 
-	$('.box').mouseleave(function( e ) {
-		
-		if (leaveCount == 1) {
-			$('.header').text("Come back");
-			leaveCount = 2;
-		} else if (leaveCount == 2) {
-			$('.header').text("I miss you already");
-			leaveCount = 3;
-		} else if (leaveCount == 3) {
-			$('.header').text("It's so cold");
-			leaveCount = 4;
-		} else if (leaveCount == 4) {
-			$('.header').text("Where do you go without me?");
-			leaveCount = 0;
-		} else {
-			$('.header').text("Where did you go?");
-			leaveCount = 1;
-		}
-	});
-}
+//	$('.box').mouseleave(function( e ) {
+//		
+//		if (leaveCount == 1) {
+//			$('.header').text("Come back");
+//			leaveCount = 2;
+//		} else if (leaveCount == 2) {
+//			$('.header').text("I miss you already");
+//			leaveCount = 3;
+//		} else if (leaveCount == 3) {
+//			$('.header').text("It's so cold");
+//			leaveCount = 4;
+//		} else if (leaveCount == 4) {
+//			$('.header').text("Where do you go without me?");
+//			leaveCount = 0;
+//		} else {
+//			$('.header').text("Where did you go?");
+//			leaveCount = 1;
+//		}
+//	});
+//}
